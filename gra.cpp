c5e82cb8f16c5c4e07b7bef1f17e6da350a44d34
lora1355 <laura.maja.terlecka@gmail.com>
2026-06-02 08:49:50 +0200

    plansza

---
+++ gra.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "=================================\n";
+cout << "      ZBIERANIE MONET\n";
+cout << "      Autor: Laura Terlecka\n";
+cout << "=================================\n";
+const int ROZMIAR = 5;
+char plansza[ROZMIAR][ROZMIAR]=
+{   {'G', '.', '.', '.', '.'},
+    {'.', 'M', '.', '.', '.'},
+    {'.', '.', 'M', '.', '.'},
+    {'.', '.', '.', 'M', '.'},
+    {'.', '.', '.', '.', 'M'}}
+}
